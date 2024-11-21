138336fa75a900198d8a59484f51ad3ddaae770a
DELL <DELL@192.168.1.8>
2024-11-21 23:56:47 +0530

    commit 3

---
+++ src/com/lbrce/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.lbrce;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Initialize the numbers
+        int number1 = 1;
+        int number2 = 2;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Display the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
